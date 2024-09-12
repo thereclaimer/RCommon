e13028b3359decc462b707f368557e7fd328d67b
Sam Davenport <sam@davenportdronographics.com>
2024-09-11 21:13:55 -0500

    added basic types

---
+++ include/r-common-scopes.hpp
@@ -0,0 +1,8 @@
+#ifndef R_COMMON_SCOPES_HPP
+#define R_COMMON_SCOPES_HPP
+
+#define internal static
+#define local    static
+#define global   static
+ 
+#endif //R_COMMON_SCOPES_HPP
---
+++ include/r-common.hpp
@@ -0,0 +1,6 @@
+#ifndef R_COMMON_HPP
+#define R_COMMON_HPP
+
+#include "r-common-types.hpp"
+
+#endif //R_COMMON_HPP
